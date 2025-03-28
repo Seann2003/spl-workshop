208ea06d53f1da8dfa9dfb7d3487c0317fa494ea
Sean Hoe Kai Zher <102246906+Seann2003@users.noreply.github.com>
2025-03-28 19:46:11 +0800

    Removed unnecessary line

--- src/token-with-image.ts
+++ src/token-with-image.ts
@@ -1,5 +1,6 @@
 import {
   createAndMint,
+  mplTokenMetadata,
   TokenStandard,
 } from "@metaplex-foundation/mpl-token-metadata";
 import {
@@ -11,13 +12,10 @@ import {
 import { readFileSync } from "fs";
 import { setupUmiClient } from "../utils/setupClient.ts";
 import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
-import { mplCandyMachine } from "@metaplex-foundation/mpl-candy-machine";
 
-// Example metadata
-// Call upon Umi's `uploadJson` function to upload our metadata to Arweave via Irys.
 (async () => {
   const { umi } = setupUmiClient();
-  umi.use(mplCandyMachine()).use(irysUploader());
+  umi.use(mplTokenMetadata()).use(irysUploader());
   const mint = generateSigner(umi);
   const imageFile = readFileSync("");
 
